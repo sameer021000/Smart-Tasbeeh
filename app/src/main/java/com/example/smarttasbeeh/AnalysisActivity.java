051f2665ecf966eff393328f9579cb42087c1ece
Sameer Shaik <sameer021000@gmail.com>
2026-02-19 15:19:05 +0530

    Generated a Signed Apk file for release in GitHub with version-v1.0.0

--- app/src/main/java/com/example/smarttasbeeh/AnalysisActivity.java
+++ app/src/main/java/com/example/smarttasbeeh/AnalysisActivity.java
@@ -2,7 +2,6 @@ package com.example.smarttasbeeh;
 
 import android.content.Context;
 import android.content.Intent;
-
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.os.Build;
